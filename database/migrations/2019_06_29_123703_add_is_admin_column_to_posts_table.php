43829e07494be25aa32c8420bb99e7fb2ea430c6
danielgontar <daniel@sogo.co.il>
2019-06-29 16:03:55 +0300

    ..

--- database/migrations/2019_06_29_123703_add_is_admin_column_to_posts_table.php
+++ database/migrations/2019_06_29_123703_add_is_admin_column_to_posts_table.php
@@ -14,7 +14,7 @@ class AddIsAdminColumnToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->tinyInteger('is_admin')->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddIsAdminColumnToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_admin');
         });
     }
 }
